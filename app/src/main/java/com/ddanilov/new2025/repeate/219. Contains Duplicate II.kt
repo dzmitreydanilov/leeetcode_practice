e1d4d160b1a41cc9c5ac8959e72627c85459844f
dzmitry.danilau <122111315+dzmitreydanilov@users.noreply.github.com>
2025-04-17 22:33:25 +0200

    17.04.25

---
+++ app/src/main/java/com/ddanilov/new2025/repeate/219. Contains Duplicate II.kt
@@ -0,0 +1,40 @@
+package com.ddanilov.new2025.repeate
+
+fun main() {
+    val nums = intArrayOf(1, 2, 3, 1)
+    val k = 3
+
+    val result = containsNearbyDuplicate(nums, k)
+
+    println("XXX $result")
+}
+
+/**
+ * 1,2,3,1,2,3
+ * l
+ *       r
+ *
+ * l = 0
+ * k = 2
+ *
+ * seen = [1,2,]
+ */
+private fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+    val seen = mutableSetOf<Int>()
+    var l = 0
+
+    for (r in nums.indices) {
+
+        if (r - l > k) {
+            seen.remove(nums[l])
+            l++
+        }
+
+        if (seen.contains(nums[r])) {
+            return true
+        } else {
+            seen.add(nums[r])
+        }
+    }
+    return false
+}
