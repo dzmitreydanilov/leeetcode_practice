44dad07913cb4726cbec1f9c16550a2506c61f58
dzmitry.danilau <122111315+dzmitreydanilov@users.noreply.github.com>
2025-04-21 23:06:46 +0200

    21.04.25

--- app/src/main/java/com/ddanilov/new2025/TeemoAttacking.kt
+++ app/src/main/java/com/ddanilov/new2025/TeemoAttacking.kt
@@ -1,24 +1,24 @@
 package com.ddanilov.new2025
 
 fun main() {
-    val nums = intArrayOf(1, 4)
-    val duration = 2
+    val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val duration = 1
 
     val result = findPoisonedDuration(nums, duration)
 
     println("XXX $result")
 }
 
-
 /**
- * 1, 4
+ * 1, 2, 3, 4, 5, 6, 7, 8, 9
  *
+ * d = 2
  */
 private fun findPoisonedDuration(timeSeries: IntArray, duration: Int): Int {
     var result = 0
-
-    for (i in timeSeries.indices - 1) {
-        result += minOf(timeSeries[i + 1] - timeSeries[i], duration)
+    for (i in 0 until timeSeries.lastIndex) {
+        val temp =timeSeries[i + 1]
+        result += minOf(temp - timeSeries[i], duration)
     }
 
     return result + duration
---
+++ app/src/main/java/com/ddanilov/new2025/repeate/496. Next Greater Element I.kt
@@ -0,0 +1,52 @@
+package com.ddanilov.new2025.repeate
+
+import java.util.Stack
+
+fun main() {
+
+    val nums1 = intArrayOf(4, 1, 2)
+    val nums2 = intArrayOf(1, 3, 4, 2)
+
+    val result = nextGreaterElement(nums1, nums2)
+
+
+    result.forEach {
+        println(it)
+    }
+}
+
+/**
+ * 4, 1, 2
+ *
+ * 1, 3, 4, 2
+ *
+ * result = [-1,3,-1]
+ *stack = [1,]
+ */
+
+private fun nextGreaterElement(nums1: IntArray, nums2: IntArray): IntArray {
+    val mapId = mutableMapOf<Int, Int>()
+
+    nums1.forEachIndexed { index, i ->
+        mapId[i] = index
+    }
+
+    val stack = Stack<Int>()
+    val result = IntArray(nums1.size) { -1 }
+
+
+    for (num in nums2) {
+        val current = num
+        while (stack.isNotEmpty() && current > stack.lastElement()) {
+            val value = stack.pop()
+            val index = mapId[value]!!
+            result[index] = current
+        }
+
+        if (num in mapId) {
+            stack.push(num)
+        }
+    }
+
+    return result
+}
