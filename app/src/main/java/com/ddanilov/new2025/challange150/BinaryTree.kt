382cd75c17afe02fdd4e4f88b778f0a2cc67161f
dzmitry.danilau <122111315+dzmitreydanilov@users.noreply.github.com>
2025-05-17 14:55:42 +0200

    16.05.25

---
+++ app/src/main/java/com/ddanilov/new2025/challange150/BinaryTree.kt
@@ -0,0 +1,79 @@
+package com.ddanilov.new2025.challange150
+
+class BinaryTree {
+    var root: Node? = null
+
+    fun insert(value: Int) {
+        val newNode = Node(value)
+        if (root == null) {
+            root = newNode
+        } else {
+            var currentNode = root
+            while (true) {
+                if (value < (currentNode?.value ?: 0)) {
+                    // Left
+                    if (currentNode?.left == null) {
+                        currentNode?.left = newNode
+                        return
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    // Right > or ==
+                    if (currentNode?.right == null) {
+                        currentNode?.right = newNode
+                        return
+                    }
+                    currentNode = currentNode.right
+                }
+            }
+        }
+    }
+
+    fun lookUp(value: Int): Node? {
+        if (root == null) {
+            return null
+        }
+
+        var currentNode = root
+
+        while (currentNode != null) {
+            currentNode = if (value < currentNode.value) {
+                currentNode.left
+            } else if (value > currentNode.value) {
+                currentNode.right
+            } else {
+                return currentNode
+            }
+        }
+        return null
+    }
+}
+
+fun printTree(node: Node?, indent: String = "", isRight: Boolean = true) {
+    if (node == null) return
+
+    printTree(node.right, indent + if (isRight) "        " else " |      ", true)
+    println(indent + (if (isRight) " /" else " \\") + "----- ${"%3d".format(node.value)}")
+    printTree(node.left, indent + if (isRight) " |      " else "        ", false)
+}
+
+class Node(val value: Int) {
+    var left: Node? = null
+    var right: Node? = null
+}
+
+fun main() {
+    val tree = BinaryTree()
+    tree.insert(10)
+    tree.insert(5)
+    tree.insert(15)
+    tree.insert(3)
+    tree.insert(7)
+    tree.insert(12)
+    tree.insert(18)
+
+   val result = tree.lookUp(18)
+
+    println("XXX ${result?.value}")
+
+}
